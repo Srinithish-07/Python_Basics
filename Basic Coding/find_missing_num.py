bc4862f278c6bf3a712e36a59754dce94b53215c
Srinithish SM <157304330+Srinithish-07@users.noreply.github.com>
2024-03-07 21:26:43 +0530

    Add files via upload

---
+++ Basic Coding/find_missing_num.py
@@ -0,0 +1,19 @@
+def find_missing_digits(phone_number):
+    phone_digits = set(str(phone_number)) # difference in set can give the missing nums in given input
+    all_digits = set('0123456789')
+    missing_digits = all_digits  - phone_digits
+    return missing_digits
+
+try:
+    phone_number = int(input("Enter the phone number : "))
+    missing_digits = find_missing_digits(phone_number)
+
+    if missing_digits:
+        print(f"Missing digits : {','.join(sorted(missing_digits))}")
+    else:
+        print("All numbers are present in the phone number")
+
+except ValueError:
+    print("Invalid input , enter a valid phone number")
+
+    
