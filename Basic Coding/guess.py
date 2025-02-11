f9b3d832a489e3405dd13cba34269500b5cc37e7
Srinithish SM <157304330+Srinithish-07@users.noreply.github.com>
2025-02-11 10:22:10 +0530

    Add files via upload

---
+++ Basic Coding/guess.py
@@ -0,0 +1,12 @@
+import random as r
+
+def guess_num():
+    user_input = int(input("\nGuess the number (1 to 10) : "))
+    random_num = r.randint(1,10)
+    if user_input == random_num:
+        print("Hooray! , u guessed correctly")
+    else:
+        print("Oops wrong Guess , Try again..")
+        guess_num() #recursively called to avoid again running the code
+
+guess_num()
