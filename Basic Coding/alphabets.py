89f2736537042eb6ac18687a8e45fc7e3416acdf
Srinithish SM <157304330+Srinithish-07@users.noreply.github.com>
2025-02-12 18:59:31 +0530

    Update alphabets.py

--- Basic Coding/alphabets.py
+++ Basic Coding/alphabets.py
@@ -4,7 +4,7 @@ string = input("Enter  a string : ")
 vowels = 0
 consonants = 0
 for char in string:
-    if char.lower() in 'aeiou': 
+    if char.lower() in 'aeiou': # it parses through aeiou one by one
         vowels += 1
     elif char.isalpha():
         consonants += 1 
