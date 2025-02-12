2beccc9ea164d04d041b6698d2734dbb20d0df89
Srinithish SM <157304330+Srinithish-07@users.noreply.github.com>
2025-02-12 18:54:16 +0530

    Update 3digits.py

--- Basic Coding/3digits.py
+++ Basic Coding/3digits.py
@@ -1,5 +1,5 @@
 #Python program to find numbers between 100 and 400 (both included) where each digit of a number is an even number. The numbers obtained should be printed in a comma-separated sequence.
-for i in range(100,401):
+for i in range(100,400 + 1):
     str_num = str(i)
     if(int(str_num[0])%2==0 and int(str_num[1])%2==0 and int(str_num[2])%2==0):
         print(str_num,end=',')
