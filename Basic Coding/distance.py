a015e052008ec5cba311761df65e023f42df4b3e
Srinithish SM <157304330+Srinithish-07@users.noreply.github.com>
2025-02-14 20:13:02 +0530

    Update distance.py

--- Basic Coding/distance.py
+++ Basic Coding/distance.py
@@ -1,12 +1,12 @@
 '''distance between two points using x1,x2,y1,y2 variables'''
-import math as m
+import math
 '''def distance():
     x1 = int(input("Enter x1 : "))
     y1 = int(input("Enter y1 : "))
     x2 = int(input("Enter x2 : "))
     y2 = int(input("Enter y2 : "))
 
-    distance = round(m.sqrt((x2-x1)**2 + (y2-y1)**2),4)
+    distance = round(math.sqrt((x2-x1)**2 + (y2-y1)**2),4)
 
     return distance
 
@@ -17,8 +17,8 @@ def distance():
     p1 = [int(input(f"Enter element {i} of p1 : "))for i in range(2)]
     p2 = [int(input(f"Enter element {i} of p2 : "))for i in range(2)]
 
-    distance = round(m.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2),4)
-    print("The distance between the points {} and {} are : {}".format(p1,p2,distance))
+    distance = round(math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2),4)
+    print(f"The distance between the points {p1} and {p2} are : {distance}")
     return 0
 
 print(distance())
