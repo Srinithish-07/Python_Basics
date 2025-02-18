b61f975a5fcb73e12e1ca9a18076f6a7e0638b99
Srinithish SM <157304330+Srinithish-07@users.noreply.github.com>
2025-02-18 19:46:13 +0530

    Update list_greaterthan.py

--- Basic Coding/list_greaterthan.py
+++ Basic Coding/list_greaterthan.py
@@ -1,18 +1,18 @@
 def greater_than(key,my_list):
     # Edge case to handle if the key is not present
-    key_present = False
-    for i in my_list:
-        if key == i:
-            key_present = True
+    if key not in list:
+        print("Key not Present")
+        return
 
-        elif key > i and key_present == True:
+    key_present = False
+    for i in list:
+        if i == key:
+            key_present = True
+        elif key > i and key_present:
             print("No,all the elements are not greater than the key")
             return
-    
-    if not key_present:
-        print("key not present")
-    else:
-        print("All elements are greater than the key")
+            
+    print("All elements are greater than the key")
 
 if __name__ == "__main__":
     n = int(input("Enter number of elements in list : "))
